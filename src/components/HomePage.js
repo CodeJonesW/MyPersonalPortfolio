6fc42616cc35599baf32f2eac6041353384bbf8c
Will Jones <willjones2010@gmail.com>
2019-10-06 21:46:12 -0400

    first commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HomePage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <p>Will's Portfolio </p>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
