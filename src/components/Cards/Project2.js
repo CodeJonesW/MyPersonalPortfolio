aba047f06ea69dd6b720276df9b99a8908f9a546
Will Jones <willjones2010@gmail.com>
2019-10-08 02:52:22 -0400

    fixed carousel attributes and project title card

--- src/components/Cards/Project2.js
+++ src/components/Cards/Project2.js
@@ -11,13 +11,15 @@ import {
     Button
 } from "shards-react";
 
+
 export default function Project2() {
+
 
     return (
         <Card className="Card m-3">
             <CardHeader>CalorieTracker I/O</CardHeader>
 
-            <Carousel className="carouselItem">
+            <Carousel className="carouselItem" controls={true} interval={null} slide={true} wrap={true}>
                 <Carousel.Item >
                     <CardImg src={CalorieTracker} id="CalorieTrackerImg" />
                 </Carousel.Item>
@@ -30,7 +32,7 @@ export default function Project2() {
             </Carousel>
             <CardBody>
                 {/* <CardTitle>Lorem Ipsum</CardTitle> */}
-                <p>Calorie Tracker allows users to chart the different relationships of food intake vs exercises and display them on different types of graphs.</p>
+                <p>This application allows users to chart the different relationships of food intake vs exercises and display them on different types of graphs.</p>
                 <Button>Live Website &rarr;</Button>
             </CardBody>
         </Card>
