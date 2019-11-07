f428c907a9f3731ce6f22e3d40fc772123e3a7b2
Will Jones <willjones2010@gmail.com>
2019-11-06 23:01:29 -0500

    added profile picturea and about card to about page

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-
-
+import selfie from '../../images/profile_picture.jpg'
+import { Card, Row, Col, Container } from "react-bootstrap"
 class About extends React.Component {
 
     render() {
         return (
             <div id="About">
-                <p>Will's About </p>
+                <Row className="d-flex justify-content-center m-3">
+                    <Col md={3}>
+                        <img className="m-3" id="profilepic" src={selfie} alt="hi" />
+                    </Col>
 
-
-            </div>
+                    <Col style={{ 'margin': '130px' }} md={6}>
+                        <Card className="m-3">
+                            <p> Lorem Ipsum</p>
+                        </Card>
+                    </Col>
+                </Row>
+            </div >
         );
     }
 }
