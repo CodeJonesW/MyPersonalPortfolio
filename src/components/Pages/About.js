5988b2c891bac80a641975e45b2c249b6b9106f0
Will Jones <willjones2010@gmail.com>
2019-11-06 23:32:37 -0500

    fixed image sizing

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -7,13 +7,21 @@ class About extends React.Component {
         return (
             <div id="About">
                 <Row className="d-flex justify-content-center m-3">
-                    <Col md={3}>
+                    <Col md={5}>
                         <img className="m-3" id="profilepic" src={selfie} alt="hi" />
                     </Col>
 
-                    <Col style={{ 'margin': '130px' }} md={6}>
-                        <Card className="m-3">
-                            <p> Lorem Ipsum</p>
+                    <Col md={6} style={{ 'padding': '50px' }}>
+                        <Card className="m-3" >
+                            <Card.Body>
+                                Full stack web developer with a love for creating, problem solving, and improving user’s relationships with
+                                websites. Experience in JavaScript, React, Ruby on Rails, and a
+                                background in craft and design. Ability to work on a team comprised of different perspectives
+                                and has been recognized for synthesizing ideas in a fluid and productive way.
+                                Strives to use and continually learn the best communication techniques both in technical and interpersonal experiences.
+                                Possesses the drive and intuition to work independently. The combination of these talents can help unify cross functional teams,
+                                maintain a high level of productivity and create a positive impact.
+                            </Card.Body>
                         </Card>
                     </Col>
                 </Row>
