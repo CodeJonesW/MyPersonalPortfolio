f7360dd291a42be9732caf08f5b16f962d55008a
Will Jones <willjones2010@gmail.com>
2019-11-18 23:09:47 -0500

    added gmail logo to contact page with link

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, Row, Col, Container } from "react-bootstrap"
 import github from '../../images/Github_Logo.png'
+import gmail from '../../images/Gmail_logo.png'
 class Contact extends React.Component {
 
     render() {
@@ -13,10 +14,18 @@ class Contact extends React.Component {
                         </a>
                     </Col>
 
+                    <Col md={5}>
+                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=williamjonescodes@gmail.com">
+                            <img id="gmailLogo" src={gmail} />
+                        </a>
+                    </Col>
+
                     <Col md={6} style={{ 'padding': '50px' }}>
                         <Card className="m-3" >
                             <Card.Body>
                                 Contact Info
+                                <br />
+                                Email: williamjonescodes@gmail.com
                             </Card.Body>
                         </Card>
                     </Col>
