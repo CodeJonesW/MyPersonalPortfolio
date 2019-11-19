93d6f9646b4673466358b77282c55c229740e3ff
Will Jones <willjones2010@gmail.com>
2019-11-18 23:18:19 -0500

    added in twitter logo and link to contact page

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -2,23 +2,13 @@ import React, { Component } from 'react';
 import { Card, Row, Col, Container } from "react-bootstrap"
 import github from '../../images/Github_Logo.png'
 import gmail from '../../images/Gmail_logo.png'
+import twitter from '../../images/twitter_logo.png'
 class Contact extends React.Component {
 
     render() {
         return (
             <div id="Contact">
                 <Row className="d-flex justify-content-center m-3">
-                    <Col md={5}>
-                        <a href="https://github.com/CodeJonesW">
-                            <img id="gitHubLogo" src={github} />
-                        </a>
-                    </Col>
-
-                    <Col md={5}>
-                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=williamjonescodes@gmail.com">
-                            <img id="gmailLogo" src={gmail} />
-                        </a>
-                    </Col>
 
                     <Col md={6} style={{ 'padding': '50px' }}>
                         <Card className="m-3" >
@@ -29,6 +19,26 @@ class Contact extends React.Component {
                             </Card.Body>
                         </Card>
                     </Col>
+                </Row>
+                <Row>
+                    <Col md={3}>
+                        <a href="https://github.com/CodeJonesW">
+                            <img id="gitHubLogo" src={github} />
+                        </a>
+                    </Col>
+
+                    <Col md={3}>
+                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=williamjonescodes@gmail.com">
+                            <img id="gmailLogo" src={gmail} />
+                        </a>
+                    </Col>
+
+                    <Col md={3}>
+                        <a href="https://twitter.com/CodeWJones">
+                            <img id="twitterLogo" src={twitter} />
+                        </a>
+                    </Col>
+
                 </Row>
             </div >
         );
