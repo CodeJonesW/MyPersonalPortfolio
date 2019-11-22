19d22a4364f494ae3d9e8204d089d5da8088fffc
Will Jones <willjones2010@gmail.com>
2019-11-22 02:37:26 -0500

    Bug Invaders project added

---
+++ src/components/ProjectCards/Project4.js
@@ -0,0 +1,42 @@
+import React from "react";
+import bug1 from '../../images/buginvaders/bug_shots.png'
+
+
+import { Carousel, ButtonGroup, Button } from 'react-bootstrap'
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardImg,
+    CardBody,
+    CardFooter
+} from "shards-react";
+
+
+export default function Project4() {
+
+
+    return (
+        <Card className="Card m-3">
+            <CardHeader>Bug Invaders</CardHeader>
+
+            <Carousel className="carouselItem" controls={false} interval={null} slide={false} wrap={false}>
+                <Carousel.Item >
+                    <CardImg src={bug1} className="bugInvaderImg" />
+                </Carousel.Item>
+
+            </Carousel>
+            <CardBody>
+                <p> A single page app that replicates a classic space shooter arcade game, keeps score, counts lives.</p>
+
+
+
+                <a href="https://bug-invaders.web.app/" target="_blank">
+                    <button size='sm' className="myButton" >Live Website &rarr;</button>
+                </a>
+
+            </CardBody>
+
+        </Card>
+    );
+}
