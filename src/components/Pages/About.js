c5f3f1d3c4303ab01ae4216c9e53b3a3b895f7c3
Will Jones <willjones2010@gmail.com>
2019-12-26 20:58:02 -0500

    typo spelling

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -57,7 +57,7 @@ class About extends React.Component {
                                 As a full-stack web developer, I love creating, problem solving, and improving website experiences.
                                 I have written code in a multitude of languages and am always striving to learn more. I currently work for CSTech
                                 developing ecommerce backend architechure and provide tutoring assitance to
-                                bootcamp students learning to code. Consistantly improving  my communication techniques in regards to
+                                bootcamp students learning to code. Consistently improving  my communication techniques in regards to
                                 software is an important ongoing goal of mine. I am driven to work independently, but also believe
                                 team collaboration is a fundamental part of finding the best solutions.
                             </Card.Body>
