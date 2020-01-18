00326df3bfdbb74f0d3f9ed77521b3214b2536d3
Will Jones <willjones2010@gmail.com>
2020-01-18 15:46:50 -0500

    added margin to calendar

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -67,7 +67,7 @@ class About extends React.Component {
                     </Col>
                 </Row>
                 <Row className="d-flex justify-content-center m-3">
-                    <Col md={12} style={{ 'padding': '250px' }}>
+                    <Col md={12} style={{ 'padding': '250px', 'margin': '20px' }}>
                         <div class="calendar">
 
                         </div>
