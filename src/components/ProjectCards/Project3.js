8a41b864c29781a2c59d65d3b93d62a4a185c85d
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-03-12 23:55:52 -0500

    updated urls

--- src/components/ProjectCards/Project3.js
+++ src/components/ProjectCards/Project3.js
@@ -1,47 +1,50 @@
 import React from "react";
-import jamUpPic1 from '../../images/jamUp/Screen Shot 2021-03-07 at 11.16.34 PM.png'
-import jamUpPic2 from '../../images/jamUp/Screen Shot 2021-03-08 at 1.43.41 PM.png'
+import jamUpPic1 from "../../images/jamUp/Screen Shot 2021-03-07 at 11.16.34 PM.png";
+import jamUpPic2 from "../../images/jamUp/Screen Shot 2021-03-08 at 1.43.41 PM.png";
 
-import { Carousel, ButtonGroup, Button } from 'react-bootstrap'
+import { Carousel, ButtonGroup, Button } from "react-bootstrap";
 import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardImg,
-    CardBody,
-    CardFooter
+  Card,
+  CardHeader,
+  CardTitle,
+  CardImg,
+  CardBody,
+  CardFooter,
 } from "shards-react";
 
-
 export default function Project2() {
+  return (
+    <Card className="Card m-3">
+      <CardHeader>JamUp</CardHeader>
 
+      <Carousel
+        className="carouselItem"
+        controls={true}
+        interval={null}
+        slide={true}
+        wrap={true}
+      >
+        <Carousel.Item>
+          <CardImg src={jamUpPic2} className="brewsterImg" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <CardImg src={jamUpPic1} className="brewsterImg" />
+        </Carousel.Item>
+      </Carousel>
+      <CardBody>
+        <p>React, Firebase Auth, Express, mySQL Musician Meetup App</p>
 
-    return (
-        <Card className="Card m-3">
-            <CardHeader>JamUp</CardHeader>
-
-            <Carousel className="carouselItem" controls={true} interval={null} slide={true} wrap={true}>
-                <Carousel.Item >
-                    <CardImg src={jamUpPic2} className="brewsterImg" />
-                </Carousel.Item>
-                <Carousel.Item >
-                    <CardImg src={jamUpPic1} className="brewsterImg" />  
-                </Carousel.Item>
-              
-            </Carousel>
-            <CardBody>
-                <p>React, Firebase Auth, Express, mySQL Musician Meetup App</p>
-
-                <a href="https://jamupdev.firebaseapp.com/" target="_blank">
-                    <button size='sm' className="myButton" >Live Website &rarr;</button>
-                </a>
-                <a href="https://github.com/CodeJonesW/JamUp" target="_blank">
-                    <button size='sm' className="myButton" >Github &rarr;</button>
-                </a>
-
-
-            </CardBody>
-
-        </Card>
-    );
+        <a href="https://jamup.org/" target="_blank">
+          <button size="sm" className="myButton">
+            Live Website &rarr;
+          </button>
+        </a>
+        <a href="https://github.com/CodeJonesW/JamUp" target="_blank">
+          <button size="sm" className="myButton">
+            Github &rarr;
+          </button>
+        </a>
+      </CardBody>
+    </Card>
+  );
 }
--- src/components/ProjectCards/Project5.js
+++ src/components/ProjectCards/Project5.js
@@ -1,49 +1,50 @@
 import React from "react";
-import standup1 from '../../images/standUp.png'
-import standup2 from '../../images/standUp2.png'
+import standup1 from "../../images/standUp.png";
+import standup2 from "../../images/standUp2.png";
 
-
-import { Carousel, ButtonGroup, Button } from 'react-bootstrap'
+import { Carousel, ButtonGroup, Button } from "react-bootstrap";
 import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardImg,
-    CardBody,
-    CardFooter
+  Card,
+  CardHeader,
+  CardTitle,
+  CardImg,
+  CardBody,
+  CardFooter,
 } from "shards-react";
 
-
 export default function Project4() {
+  return (
+    <Card className="Card m-3">
+      <CardHeader>standUp</CardHeader>
 
+      <Carousel
+        className="carouselItem"
+        controls={true}
+        interval={null}
+        slide={true}
+        wrap={true}
+      >
+        <Carousel.Item>
+          <CardImg src={standup2} className="bugInvaderImg" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <CardImg src={standup1} className="bugInvaderImg" />
+        </Carousel.Item>
+      </Carousel>
+      <CardBody>
+        <p> React, Node, Express, Sequelize Developer StandUp app</p>
 
-    return (
-        <Card className="Card m-3">
-            <CardHeader>standUp</CardHeader>
-
-            <Carousel className="carouselItem" controls={true} interval={null} slide={true} wrap={true}>
-                <Carousel.Item >
-                    <CardImg src={standup2} className="bugInvaderImg" />
-                </Carousel.Item>
-                <Carousel.Item >
-                    <CardImg src={standup1} className="bugInvaderImg" />
-                </Carousel.Item>
-
-            </Carousel>
-            <CardBody>
-                <p> React, Node, Express, Sequelize Developer StandUp app</p>
-
-
-
-                <a href="https://standup-eba73.firebaseapp.com/" target="_blank">
-                    <button size='sm' className="myButton" >Live Website &rarr;</button>
-                </a>
-                <a href="https://github.com/CodeJonesW/standUp" target="_blank">
-                    <button size='sm' className="myButton" >Github &rarr;</button>
-                </a>
-
-            </CardBody>
-
-        </Card>
-    );
+        <a href="https://standuporganizer.com/" target="_blank">
+          <button size="sm" className="myButton">
+            Live Website &rarr;
+          </button>
+        </a>
+        <a href="https://github.com/CodeJonesW/standUp" target="_blank">
+          <button size="sm" className="myButton">
+            Github &rarr;
+          </button>
+        </a>
+      </CardBody>
+    </Card>
+  );
 }
